af9b21658cbd53523c7717500700d14193d1053e
hoed <hoedhud@gmail.com>
2021-03-24 00:39:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,33 @@ setenforce 0
 sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
 yum install -y httpd
 yum install -y mariadb-server
+
+systemctl restart httpd
+systemctl restart mariadb
+
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=443/tcp --permanent
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=https
+firewall-cmd --reload
+
+hostname -I
+
+mkdir -p /var/www/example.com/public_html
+mkdir -p /var/www/example.com/log
+chown -R $USER:$USER /var/www/example.com/public_html
+
+chmod -R 755 /var/www
+
+
+sed -i "s/^<html>
+  <head>
+    <title>Welcome to Example.com!</title>
+  </head>
+  <body>
+    <h1>Success! The example.com virtual host is working!</h1>
+  </body>
+</html>" /var/www/example.com/html/index.html
+
+mkdir /etc/httpd/sites-available /etc/httpd/sites-enabled
+sed -i "s/^IncludeOptional sites-enabled/*.conf/g" /etc/httpd/conf/httpd.conf
